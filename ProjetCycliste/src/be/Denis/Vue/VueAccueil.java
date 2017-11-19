2f09710d9ca4fecc3f83ef25ff51d8451cedbf63
Denis PAIMPARET <denis.paimparet@condorcet.be>
2017-11-19 16:20:58 +0100

    Modification de la vueAccueil

--- ProjetCycliste/src/be/Denis/Vue/VueAccueil.java
+++ ProjetCycliste/src/be/Denis/Vue/VueAccueil.java
@@ -13,13 +13,23 @@ import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 public class VueAccueil {
 
-	private JFrame frame;
+	private JFrame ecranAccueil;
 	private JTextField textUserName;
 	private JPasswordField password;
+	private JLabel lblAccueil;
+	private JLabel lblLogin;
+	private JLabel lblPassword;
+	private JButton btnConnexion;
+	private JButton btnReset;
+	private JLabel lblErreur;
+	private JButton btnInscription;
+	private JButton btnClose;
+	
 
 	/**
 	 * Launch the application.
@@ -29,7 +39,7 @@ public class VueAccueil {
 			public void run() {
 				try {
 					VueAccueil window = new VueAccueil();
-					window.frame.setVisible(true);
+					window.ecranAccueil.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -42,104 +52,134 @@ public class VueAccueil {
 	 */
 	public VueAccueil() {
 		initialize();
+		connexion();
 	}
-
+	
 	/**
-	 * Initialize the contents of the frame.
+	 * Initialisation de ma vue
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.CYAN);
-		frame.setBounds(200, 200, 500, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+
+		ecranAccueil = new JFrame();
+		ecranAccueil.getContentPane().setBackground(Color.CYAN);
+		ecranAccueil.setBounds(200, 200, 500, 300);
+		ecranAccueil.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ecranAccueil.getContentPane().setLayout(null);
 		
-		JLabel lblAccueil = new JLabel("Club Cycliste du Pays Noir");
+		lblAccueil = new JLabel("Club Cycliste du Pays Noir");
 		lblAccueil.setForeground(Color.BLUE);
 		lblAccueil.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
 		lblAccueil.setBackground(Color.WHITE);
 		lblAccueil.setBounds(122, 11, 274, 30);
-		frame.getContentPane().add(lblAccueil);
+		ecranAccueil.getContentPane().add(lblAccueil);
 		
-		JLabel lblLogin = new JLabel("Utilisateur");
+		lblLogin = new JLabel("Utilisateur");
 		lblLogin.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblLogin.setForeground(Color.BLUE);
 		lblLogin.setBounds(65, 72, 73, 17);
-		frame.getContentPane().add(lblLogin);
+		ecranAccueil.getContentPane().add(lblLogin);
 		
-		JLabel lblPassword = new JLabel("Mot de passe");
+		lblPassword = new JLabel("Mot de passe");
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblPassword.setForeground(Color.BLUE);
 		lblPassword.setBounds(65, 100, 99, 14);
-		frame.getContentPane().add(lblPassword);
+		ecranAccueil.getContentPane().add(lblPassword);
 		
 		textUserName = new JTextField();
 		textUserName.setBounds(168, 72, 152, 20);
-		frame.getContentPane().add(textUserName);
+		ecranAccueil.getContentPane().add(textUserName);
 		textUserName.setColumns(10);
 		
 		password = new JPasswordField();
 		password.setBounds(168, 99, 152, 20);
-		frame.getContentPane().add(password);
+		ecranAccueil.getContentPane().add(password);
 		
-		//Bouton de connection
-		JButton btnConnexion = new JButton("Connection");
-		btnConnexion.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				String motDePasse = password.getText();
-				String utilisateur = textUserName.getText();
-				
-				
-				
-			}
-		});
+		btnConnexion = new JButton("Connection");
 		btnConnexion.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnConnexion.setBackground(Color.GREEN);
 		btnConnexion.setForeground(Color.BLUE);
 		btnConnexion.setBounds(55, 152, 99, 23);
-		frame.getContentPane().add(btnConnexion);
+		ecranAccueil.getContentPane().add(btnConnexion);
 		
-		JButton btnReset = new JButton("Reset");
+		btnReset = new JButton("Reset");
+		btnReset.setBackground(new Color(255, 200, 0));
+		btnReset.setForeground(Color.BLUE);
+		btnReset.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnReset.setBounds(195, 152, 89, 23);
+		ecranAccueil.getContentPane().add(btnReset);
+		
+		btnInscription = new JButton("Inscription");
+		btnInscription.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnInscription.setBackground(Color.LIGHT_GRAY);
+		btnInscription.setForeground(Color.BLUE);
+		btnInscription.setBounds(349, 152, 99, 23);
+		ecranAccueil.getContentPane().add(btnInscription);
+		
+		btnClose = new JButton("Quitter");
+		btnClose.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnClose.setBackground(Color.RED);
+		btnClose.setForeground(Color.WHITE);
+		btnClose.setBounds(168, 227, 117, 23);
+		ecranAccueil.getContentPane().add(btnClose);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(21, 52, 453, 2);
+		ecranAccueil.getContentPane().add(separator_1);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 206, 453, 2);
+		ecranAccueil.getContentPane().add(separator);
+		
+		lblErreur = new JLabel("");
+		lblErreur.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblErreur.setForeground(Color.RED);
+		lblErreur.setBounds(330, 75, 144, 44);
+		ecranAccueil.getContentPane().add(lblErreur);
+	}
+		
+		/***
+		 * Connexion
+		 */
+		
+		private void connexion() {
+			btnConnexion.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ev){	
+				if(textUserName != null && password != null){
+					//Client c = new Client(login.getText(),pass.getText());
+					try {
+						if(){
+							//screenLogin.dispose();
+						}else {
+							lblErreur.setText("login ou mot de passe incorrect");
+						}
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+					
+			}	
+		});
+			
+		/***
+		 * Remettre les champs de connexion à vide
+		 */
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textUserName.setText(null);
 				password.setText(null);
 			}
 		});
-		btnReset.setBackground(new Color(255, 200, 0));
-		btnReset.setForeground(Color.BLUE);
-		btnReset.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnReset.setBounds(195, 152, 89, 23);
-		frame.getContentPane().add(btnReset);
-		
-		JButton btnInscription = new JButton("Inscription");
-		btnInscription.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnInscription.setBackground(Color.LIGHT_GRAY);
-		btnInscription.setForeground(Color.BLUE);
-		btnInscription.setBounds(349, 152, 99, 23);
-		frame.getContentPane().add(btnInscription);
-		
-		JButton btnClose = new JButton("Quitter");
+		/***
+		 * Quitter l'application
+		 */
 		btnClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame = new JFrame("Quitter");
-				if(JOptionPane.showConfirmDialog(frame, "Voulez-vous quitter?","Le programme",JOptionPane.YES_NO_OPTION)== JOptionPane.YES_NO_OPTION)
+				ecranAccueil = new JFrame("Quitter");
+				if(JOptionPane.showConfirmDialog(ecranAccueil, "Voulez-vous quitter?","Le programme",JOptionPane.YES_NO_OPTION)== JOptionPane.YES_NO_OPTION)
 					System.exit(0);
 			}
 		});
-		btnClose.setFont(new Font("Tahoma", Font.BOLD, 18));
-		btnClose.setBackground(Color.RED);
-		btnClose.setForeground(Color.WHITE);
-		btnClose.setBounds(168, 227, 117, 23);
-		frame.getContentPane().add(btnClose);
-		
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(21, 52, 453, 2);
-		frame.getContentPane().add(separator_1);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 206, 453, 2);
-		frame.getContentPane().add(separator);
 	}
 }
